660e85de951993966fedb5a83fd1fc8a9dd62d61
KingQuinn <khadijahyunusa6@gmail.com>
2022-11-14 00:06:30 +0100

    first commit

---
+++ studentID.py
@@ -0,0 +1,10 @@
+#student id
+def student_id(name, matric_no, department):
+    print('Hello',name+'!!!!\nWelcome to Nucleus\nYour student ID is:')
+    print(name)
+    print(matric_no)
+    print(department)
+    
+student_id("Khadijah", "21/sci01/072", "Computer science" )
+    
+    
